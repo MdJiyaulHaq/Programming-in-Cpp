22613559cc96f007ca371ae8be1cce147a6e8dc4
Zia-ul-haq <muhammadziaulhaq78692@gmail.com>
2021-03-19 16:28:17 +0545

    formated document

--- intro.cpp
+++ intro.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    cout<<"\n We surely belong to Allah and to Him we shall return."<<endl;
+    cout << "\n We surely belong to Allah and to Him we shall return." << endl;
 }
--- oop.cpp
+++ oop.cpp
@@ -1,48 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 
 class student
 {
-    public:
-    string  name;
-    int     roll;
-    bool    gender;
-    
+public:
+    string name;
+    int roll;
+    bool gender;
+
     void printInfo()
     {
-        cout<<"name:";
-        cout<<name<<endl;
-        cout<<"roll:";
-        cout<<roll<<endl;
-        cout<<"gender:";
-        cout<<gender<<endl;
-
+        cout << "name:";
+        cout << name << endl;
+        cout << "roll:";
+        cout << roll << endl;
+        cout << "gender:";
+        cout << gender << endl;
     }
 };
 
 int main()
 {
     int count;
-    cout<<"For how many students you want to enter the data?"<<endl;
-    cin>>count;
+    cout << "For how many students you want to enter the data?" << endl;
+    cin >> count;
     student arr[count];
-    for (int i=0;i<count;i++)
+    for (int i = 0; i < count; i++)
     {
-        cout<<"enter the name of student:"<<i+1<<endl;
-        cin>>arr[i].name;
-        cout<<"enter the roll number of student:"<<i+1<<endl;
-        cin>>arr[i].roll;
-        cout<<"enter the gender of student:"<<i+1<<endl;
-        cin>>arr[i].gender;
-         
+        cout << "enter the name of student:" << i + 1 << endl;
+        cin >> arr[i].name;
+        cout << "enter the roll number of student:" << i + 1 << endl;
+        cin >> arr[i].roll;
+        cout << "enter the gender of student:" << i + 1 << endl;
+        cin >> arr[i].gender;
     }
     for (int i = 0; i < count; i++)
     {
-        arr[i].printInfo();  /* code */
+        arr[i].printInfo(); /* code */
     }
-    
-    
-    
+
     return 0;
 }
