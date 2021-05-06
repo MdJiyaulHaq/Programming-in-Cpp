0b2346ee86b1d4575e08129555a18a4325e3117f
Zia-ul-haq <muhammadziaulhaq78692@gmail.com>
2021-05-06 16:17:15 +0545

    corrected some error

--- dynamic_initialization_of_pointers_using_constructors.cpp
+++ dynamic_initialization_of_pointers_using_constructors.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class BankDeposit
-{
+
+class BankDeposit{
     int principal;
     int years;
     float interestRate;
     float returnValue;
 
-public:
-    BankDeposit() {}
-    BankDeposit(int p, int y, float r); // r can be a value like 0.04
-    BankDeposit(int p, int y, int r);   // r can be a value like 4
-    void show();
+    public:
+        BankDeposit(){}
+        BankDeposit(int p, int y, float r); // r can be a value like 0.04
+        BankDeposit(int p, int y, int r); // r can be a value like 14
+        void show();
 };
 
-BankDeposit ::BankDeposit(int p, int y, float r)
+BankDeposit :: BankDeposit(int p, int y, float r)
 {
     principal = p;
     years = y;
@@ -23,44 +23,42 @@ BankDeposit ::BankDeposit(int p, int y, float r)
     returnValue = principal;
     for (int i = 0; i < y; i++)
     {
-        returnValue = returnValue * (1 + interestRate);
+        returnValue = returnValue * (1+interestRate);
     }
 }
 
-BankDeposit ::BankDeposit(int p, int y, int r)
+BankDeposit :: BankDeposit(int p, int y, int r)
 {
     principal = p;
     years = y;
-    interestRate = float(r) / 100;
+    interestRate = float(r)/100;
     returnValue = principal;
     for (int i = 0; i < y; i++)
     {
-        returnValue = returnValue * (1 + interestRate);
+        returnValue = returnValue * (1+interestRate);
     }
 }
 
-void BankDeposit ::show()
-{
-    cout << endl
-         << "Principal amount was " << principal
-         << ". Return value after " << years
-         << " years is " << returnValue << endl;
+void BankDeposit :: show(){
+    cout<<endl<<"Principal amount was "<<principal
+        << ". Return value after "<<years
+        << " years is "<<returnValue<<endl;
 }
 
-int main()
-{
+int main(){
     BankDeposit bd1, bd2, bd3;
     int p, y;
     float r;
     int R;
-
-    cout << "Enter the value of p y and r" << endl;
-    cin >> p >> y >> r;
+    
+    
+    cout<<"Enter the value of p y and r"<<endl;
+    cin>>p>>y>>r;
     bd1 = BankDeposit(p, y, r);
     bd1.show();
 
-    cout << "Enter the value of p y and R" << endl;
-    cin >> p >> y >> R;
+    cout<<"Enter the value of p y and R"<<endl;
+    cin>>p>>y>>R;
     bd2 = BankDeposit(p, y, R);
     bd2.show();
     return 0;
